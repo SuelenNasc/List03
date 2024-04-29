5af72e8787dfe66d211d02237c78afd9bba00cc8
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 20:59:43 -0300

    Ex10

---
+++ br/edu/up/exercicios/Ex10.java
@@ -0,0 +1,25 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.VerificadorIdade;
+
+public class Ex10 {
+
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Digite o número de pessoas: ");
+        int numeroPessoas = scanner.nextInt();
+
+        for (int i = 1; i <= numeroPessoas; i++) {
+            System.out.print("Digite a idade da pessoa " + i + ": ");
+            int idade = scanner.nextInt();
+
+            String mensagem = VerificadorIdade.verificarMaioridade(idade);
+
+            System.out.println(mensagem);
+        }
+
+        scanner.close();
+    }
+}
