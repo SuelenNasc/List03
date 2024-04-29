544e998aad2a53035ceb3832b55d28e78f6192b3
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 20:04:00 -0300

    Ex02

---
+++ br/edu/up/exercicios/Ex02.java
@@ -0,0 +1,24 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.Automovel;
+
+public class Ex02 {
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a distância total percorrida (em km): ");
+        double distancia = scanner.nextDouble();
+
+        System.out.print("Digite o total de combustível gasto (em litros): ");
+        double combustivel = scanner.nextDouble();
+
+        Automovel automovel = new Automovel(distancia, combustivel);
+
+        double consumoMedio = automovel.calcularConsumoMedio();
+
+        System.out.println("\nConsumo médio do automóvel: " + consumoMedio + " km/l");
+
+        scanner.close();
+    }
+}
