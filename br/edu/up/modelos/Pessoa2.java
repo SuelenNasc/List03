14df115ee786f1bef343e3393d46b51295151b7e
SuelenNasc <suh_nass@hotmail.com>
2024-04-29 20:46:30 -0300

    Pessoa2

---
+++ br/edu/up/modelos/Pessoa2.java
@@ -0,0 +1,24 @@
+package br.edu.up.modelos;
+import java.util.Scanner;
+
+public class Pessoa2 {
+    private String nome;
+    private char sexo;
+    private int idade;
+    private boolean saude;
+
+    public Pessoa2(String nome, char sexo, int idade, boolean saude) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.saude = saude;
+    }
+
+    public boolean estaApta() {
+        return (idade >= 18 && saude);
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+}
