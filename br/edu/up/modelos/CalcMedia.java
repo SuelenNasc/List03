c7a9e8c914534d880cf5e3c77a20d79aeb6285f1
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 20:58:47 -0300

    CalcMedia

---
+++ br/edu/up/modelos/CalcMedia.java
@@ -0,0 +1,19 @@
+package br.edu.up.modelos;
+import java.util.Scanner;
+
+public class CalcMedia {
+   
+    public static double calcularMedia(double nota1, double nota2, double nota3) {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+    
+    public static String determinarMencao(double media) {
+        if (media >= 7) {
+            return "Aprovado";
+        } else if (media <= 5) {
+            return "Reprovado";
+        } else {
+            return "Recuperação";
+        }
+    }
+}
