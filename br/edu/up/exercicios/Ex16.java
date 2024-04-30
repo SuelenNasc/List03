c4c191027e1a0ba17473333201f0c1f128e17a99
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 21:07:37 -0300

    Ex16

---
+++ br/edu/up/exercicios/Ex16.java
@@ -0,0 +1,31 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.Funcionario;
+
+public class Ex16 {
+
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor do salário mínimo:");
+        double salarioMinimo = scanner.nextDouble();
+
+        double totalReajuste = 0;
+
+        for (int i = 1; i <= 584; i++) {
+            System.out.println("Digite o salário do funcionário " + i + ":");
+            double salario = scanner.nextDouble();
+
+            Funcionario funcionario = new Funcionario(salario, salarioMinimo);
+            double reajuste = funcionario.calcularReajuste();
+            totalReajuste += reajuste;
+
+            System.out.println("Reajuste para o funcionário " + i + ": R$" + reajuste);
+        }
+
+        System.out.println("\nTotal de reajuste dado aos funcionários: R$" + totalReajuste);
+
+        scanner.close();
+    }
+}
