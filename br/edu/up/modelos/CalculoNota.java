6d456d9a1bb2974d9175be7597329e8d86b818fb
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 21:17:42 -0300

    CalculoNota

---
+++ br/edu/up/modelos/CalculoNota.java
@@ -0,0 +1,12 @@
+package br.edu.up.modelos;
+import java.util.Scanner;
+
+public class CalculoNota {
+    public static double calcularNotaFinal(double notaLaboratorio, double notaAvaliacao, double notaExame) {
+        double pesoLaboratorio = 2;
+        double pesoAvaliacao = 3;
+        double pesoExame = 5;
+
+        return (notaLaboratorio * pesoLaboratorio + notaAvaliacao * pesoAvaliacao + notaExame * pesoExame) / (pesoLaboratorio + pesoAvaliacao + pesoExame);
+    }
+}
