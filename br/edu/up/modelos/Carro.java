6097c4b9b6c5aa10f0c2b85f531035fe95a95758
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 21:04:07 -0300

    Carro

---
+++ br/edu/up/modelos/Carro.java
@@ -0,0 +1,18 @@
+package br.edu.up.modelos;
+import java.util.Scanner;
+
+public class Carro {
+    private int ano;
+
+    public Carro(int ano) {
+        this.ano = ano;
+    }
+
+    public double calcularDesconto() {
+        if (ano <= 2000) {
+            return 0.12; 
+        } else {
+            return 0.07;
+        }
+    }
+}
