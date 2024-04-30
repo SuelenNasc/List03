063a49be0917f09e2362310b7ad0395322e74fc7
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 21:09:34 -0300

    Ex18

---
+++ br/edu/up/exercicios/Ex18.java
@@ -0,0 +1,33 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.Funcionario3;
+
+public class Ex18 {
+
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do funcionário:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a idade do funcionário:");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o sexo do funcionário (M/F):");
+        char sexo = scanner.next().charAt(0);
+        scanner.nextLine(); 
+
+        System.out.println("Digite o salário fixo do funcionário:");
+        double salarioFixo = scanner.nextDouble();
+
+        Funcionario3 funcionario = new Funcionario3(nome, idade, sexo, salarioFixo);
+        double salarioLiquido = funcionario.calcularSalarioLiquido();
+
+        System.out.println("\nNome do funcionário: " + funcionario.getNome());
+        System.out.println("Salário líquido: R$" + salarioLiquido);
+
+        scanner.close();
+    }
+}
