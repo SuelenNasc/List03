2af160966cbe3569914c36951000bccb974b2ef5
SuelenNasc <suh_nass@hotmail.com>
2024-04-29 21:21:54 -0300

    Veiculo

---
+++ br/edu/up/modelos/Veiculo.java
@@ -0,0 +1,28 @@
+package br.edu.up.modelos;
+import java.util.Scanner;
+
+public class Veiculo {
+    private String combustivel;
+
+    public Veiculo(String combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public double calcularDesconto(double valorVeiculo) {
+        double desconto = 0;
+        switch (combustivel.toLowerCase()) {
+            case "álcool":
+                desconto = valorVeiculo * 0.25;
+                break;
+            case "gasolina":
+                desconto = valorVeiculo * 0.21;
+                break;
+            case "diesel":
+                desconto = valorVeiculo * 0.14;
+                break;
+            default:
+                System.out.println("Combustível inválido.");
+        }
+        return desconto;
+    }
+}
