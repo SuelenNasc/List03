caafddadd7a9970c6c7d2819f4a84f0e6ff3abab
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 21:19:00 -0300

    Ex26

---
+++ br/edu/up/exercicios/Ex26.java
@@ -0,0 +1,24 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.Seguradora;
+
+class Ex26 {
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do pretendente:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a idade do pretendente:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite o grupo de risco do pretendente (Baixo/Médio/Alto):");
+        String grupoRisco = scanner.next();
+
+        Seguradora pessoa = new Seguradora(nome, idade, grupoRisco);
+        pessoa.determinarCategoria();
+
+        scanner.close();
+    }
+}
