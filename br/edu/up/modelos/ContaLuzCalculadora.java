41a403134443b9a85fe786de33a9b9669d0a2667
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 21:14:47 -0300

    ContaLuzCalculadora

---
+++ br/edu/up/modelos/ContaLuzCalculadora.java
@@ -0,0 +1,24 @@
+package br.edu.up.modelos;
+import java.util.Scanner;
+
+public class ContaLuzCalculadora {
+    public static double calcularContaLuz(int tipoCliente, double kWhConsumidos) {
+        double valorKWh;
+        switch (tipoCliente) {
+            case 1:
+                valorKWh = 0.602;
+                break;
+            case 2:
+                valorKWh = 0.483;
+                break;
+            case 3:
+                valorKWh = 1.29;
+                break;
+            default:
+                System.out.println("Tipo de cliente inválido. Utilizando valor padrão para residência.");
+                valorKWh = 0.602;
+        }
+
+        return valorKWh * kWhConsumidos;
+    }
+}
