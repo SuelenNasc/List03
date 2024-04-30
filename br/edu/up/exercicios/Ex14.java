cbadb7538836c74dababdfc390ff61e763b839c6
Pedrotechnine <pedro.freitastech@gmail.com>
2024-04-29 21:05:05 -0300

    Ex14

---
+++ br/edu/up/exercicios/Ex14.java
@@ -0,0 +1,39 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.Produto;
+
+public class Ex14 {
+
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in);
+
+        double somaCusto = 0;
+        double somaVenda = 0;
+        int totalProdutos = 40;
+
+        for (int i = 1; i <= totalProdutos; i++) {
+            System.out.println("Produto " + i);
+            System.out.println("Digite o preço de custo:");
+            double custo = scanner.nextDouble();
+
+            System.out.println("Digite o preço de venda:");
+            double venda = scanner.nextDouble();
+
+            Produto produto = new Produto(custo, venda);
+            String resultado = produto.verificarLucroPrejuizoEmpate();
+            System.out.println("Resultado para o Produto " + i + ": " + resultado);
+
+            somaCusto += produto.getPrecoCusto();
+            somaVenda += produto.getPrecoVenda();
+        }
+
+        double mediaCusto = somaCusto / totalProdutos;
+        double mediaVenda = somaVenda / totalProdutos;
+
+        System.out.println("\nMédia de preço de custo: " + mediaCusto);
+        System.out.println("Média de preço de venda: " + mediaVenda);
+
+        scanner.close();
+    }
+}
